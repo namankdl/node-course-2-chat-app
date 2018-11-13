0b5ff4068ddb83fd9c5ee26c614dda0e8a401ba3
namanjainkdl <namanjainkdl@gmail.com>
2018-11-13 22:39:49 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(express.static(publicPath));
 
 
-app.listen(3000,()=>{console.log(`server is up on ${port}`);
+app.listen(port,()=>{console.log(`server is up on ${port}`);
 });
 
 
